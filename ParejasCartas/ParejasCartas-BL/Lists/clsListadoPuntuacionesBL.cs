eff3a1613562b3cfb38b5ae171b182b02901764b
Daniel Gordillo Rodríguez <danielgordillo9@gmail.com>
2020-01-24 02:31:44 +0100

    gg

---
+++ ParejasCartas/ParejasCartas-BL/Lists/clsListadoPuntuacionesBL.cs
@@ -0,0 +1,21 @@
+﻿using ParejasCartas_Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParejasCartas_BL.Lists
+{
+    public class clsListadoPuntuacionesBL
+    {
+
+        public List<clsScore> obtenerListadoPuntuaciones()
+        {
+            List<clsScore> scores = new List<clsScore>();
+
+            return scores;
+        }
+
+    }
+}
---
+++ ParejasCartas/ParejasCartas-UI/ViewModels/clsJuegoVM.cs
@@ -0,0 +1,69 @@
+﻿using ParejasCartas_UI.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParejasCartas_UI.ViewModels
+{
+    public class clsJuegoVM : INotifyPropertyChanged
+    {
+
+        private ObservableCollection<clsCarta> _tablero;
+        private string _tiempo;
+        private string _nombreSeleccionado;
+
+        public clsJuegoVM()
+        {
+            //Crea el tablero con metodo de las cartas aleatorias de la clase de utilidades
+        }
+
+        public ObservableCollection<clsCarta> Tablero
+        {
+            get
+            {
+                return _tablero;
+            }
+            set
+            {
+                _tablero = value;
+            }
+        }
+
+        public string Tiempo
+        {
+            get
+            {
+                return _tiempo;
+            }
+            set
+            {
+                _tiempo = value;
+            }
+        }
+
+        public string NombreSeleccionado
+        {
+            get
+            {
+                return _nombreSeleccionado;
+            }
+            set
+            {
+                _nombreSeleccionado = value;
+            }
+        }
+
+        //Cosas del NotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
